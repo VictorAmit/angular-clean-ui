e8da71d88ddabea4eaca375dfd02f1421c525639
Mediatec Software <hello@mediatec.org>
2019-01-22 01:23:09 +0300

    removing unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { NgModule } from '@angular/core'
+import { NgModule, LOCALE_ID } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
-import { NZ_I18N, NZ_ICONS, en_US } from 'ng-zorro-antd'
 import { IconDefinition } from '@ant-design/icons-angular'
 import * as AllIcons from '@ant-design/icons-angular/icons'
 import { NgProgressModule } from '@ngx-progressbar/core'
@@ -15,17 +14,6 @@ import { AngularFirestoreModule } from '@angular/fire/firestore'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-
-// config angular i18n
-import { registerLocaleData } from '@angular/common'
-import en from '@angular/common/locales/en'
-registerLocaleData(en)
-
-// map antd icons
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition
-}
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 // firebase config
 const firebaseConfig = {
@@ -57,7 +45,6 @@ const firebaseConfig = {
     AngularFirestoreModule,
     AppRoutingModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
