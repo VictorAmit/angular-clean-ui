71ae07fe5da9f41040d47216e089e8c3f873a334
Mediatec Software <hello@mediatec.org>
2019-01-24 16:58:13 +0300

    livesearch component

--- src/app/components/LayoutComponents/TopBar/LiveSearch/live-search.component.ts
+++ src/app/components/LayoutComponents/TopBar/LiveSearch/live-search.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'cui-topbar-live-search',
   templateUrl: './live-search.component.html',
   styleUrls: ['./live-search.component.scss'],
 })
-export class TopbarLiveSearchComponent {}
+export class TopbarLiveSearchComponent {
+  @ViewChild('liveSearchInput') liveSearchInput: ElementRef
+
+  showSearch: boolean = false
+
+  constructor() {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this), false)
+  }
+
+  setVisible() {
+    this.showSearch = true
+    this.liveSearchInput.nativeElement.focus()
+  }
+
+  setHidden() {
+    this.showSearch = false
+  }
+
+  handleKeyDown(event: any) {
+    if (this.showSearch) {
+      const key = event.keyCode.toString()
+      if (key === '27') {
+        this.setHidden()
+      }
+    }
+  }
+}
