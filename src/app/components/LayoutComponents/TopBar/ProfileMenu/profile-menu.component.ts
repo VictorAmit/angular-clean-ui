5a8725cc61dcb4ae164ca44cc91ed18d41f8dff0
Mediatec Software <hello@mediatec.org>
2019-01-24 15:55:49 +0300

    topbar components / logout

--- src/app/components/LayoutComponents/TopBar/ProfileMenu/profile-menu.component.ts
+++ src/app/components/LayoutComponents/TopBar/ProfileMenu/profile-menu.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core'
+import { AuthService } from 'src/app/services/auth.service'
 
 @Component({
   selector: 'cui-topbar-profile-menu',
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss'],
 })
-export class TopbarProfileMenuComponent {}
+export class TopbarProfileMenuComponent {
+  badgeCount: number = 7
+
+  constructor(public authService: AuthService) {}
+
+  badgeCountIncrease() {
+    this.badgeCount = this.badgeCount + 1
+  }
+
+  logout() {
+    this.authService.SignOut()
+  }
+}
