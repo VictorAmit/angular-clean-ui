eb4a32dac8a079f84326a79690a2c02397cd6ac0
Mediatec Software <hello@mediatec.org>
2019-02-01 21:02:54 +0300

    new login

--- src/app/layouts/Login/login.component.ts
+++ src/app/layouts/Login/login.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LayoutLoginComponent {}
+export class LayoutLoginComponent {
+  backgroundNumber = 1
+  backgroundEnabled = false
+
+  changeBackground(): void {
+    this.backgroundEnabled = true
+    this.backgroundNumber === 5 ? (this.backgroundNumber = 1) : (this.backgroundNumber += 1)
+  }
+
+  toggleBackground(): void {
+    this.backgroundEnabled = !this.backgroundEnabled
+  }
+}
