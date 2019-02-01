f9ad8c44f2323485916b4eae38f35163b1efca52
Mediatec Software <hello@mediatec.org>
2019-02-01 16:12:41 +0300

    menu active item draft

--- src/app/components/LayoutComponents/Menu/MenuLeft/menu-left.component.ts
+++ src/app/components/LayoutComponents/Menu/MenuLeft/menu-left.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
+import { Router, NavigationStart } from '@angular/router'
+import { filter } from 'rxjs/operators'
 import { select, Store } from '@ngrx/store'
 import { MenuService } from 'src/app/services/menu.service'
 import * as SettingsActions from 'src/app/store/settings/actions'
@@ -15,8 +17,13 @@ export class MenuLeftComponent implements OnInit {
   isSettingsOpen: boolean
   isMobileView: boolean
   menuData: any[]
+  menuDataActivated: any[]
 
-  constructor(private menuService: MenuService, private store: Store<any>) {}
+  constructor(
+    private menuService: MenuService,
+    private store: Store<any>,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     this.menuService.getLeftMenuData().subscribe(menuData => (this.menuData = menuData))
@@ -24,6 +31,17 @@ export class MenuLeftComponent implements OnInit {
       this.isLightTheme = state.isLightTheme
       this.isMobileView = state.isMobileView
     })
+    this.setActiveMenuItem(this.router.url)
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationStart))
+      .subscribe((event: NavigationStart) => {
+        this.setActiveMenuItem(event.url ? event.url : null)
+      })
+  }
+
+  setActiveMenuItem(event: any) {
+    this.menuDataActivated = this.menuData
+    // TODO: set active item
   }
 
   toggleSettings() {
