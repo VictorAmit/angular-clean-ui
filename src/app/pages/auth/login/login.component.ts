98448c213c2d46dab07c0453943755cd74b5fe22
Sellpixels Robot <nick@mediatec.org>
2020-05-29 00:20:12 +0300

    login page removing unused imports

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { select, Store } from '@ngrx/store'
-import * as Reducers from 'src/app/store/reducers'
-import { AuthService } from 'src/app/services/firebase.auth.service'
 
 @Component({
   selector: 'cui-system-login-page',
